14dfd7674c12682f3ee7b870478613809489c7ae
Daniel Markbreiter <dmarkbreiter@nhm.org>
2023-07-25 16:18:53 -0700

    Removed unneeded useEffect

--- src/Form/index.tsx
+++ src/Form/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import clsx from 'clsx';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { Button } from '../Button';
 
 interface FormProps {
