bf69f4636169e6c84e8ca39a50ca896e3cdc3132
Daniel Markbreiter <dmarkbreiter@nhm.org>
2023-08-01 14:02:24 -0700

    Added onClick to SubitButton

--- src/components/SubmitButton.tsx
+++ src/components/SubmitButton.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
-import { Button, ButtonProps } from "./Button";
+import { Button, ButtonProps } from './Button';
 
 interface SubmitButtonProps extends ButtonProps {
   value?: string;
   form?: string;
 }
 
-export const SubmitButton: React.FC<SubmitButtonProps> = ({value="Submit", ...props}) => {
+export const SubmitButton: React.FC<SubmitButtonProps> = ({
+  value = 'Submit',
+  ...props
+}) => {
   return (
-    <Button variant={props.variant} disabled={props.disabled} classes={props.classes}>
+    <Button
+      variant={props.variant}
+      disabled={props.disabled}
+      classes={props.classes}
+      onClick={props.onClick}
+    >
       <input type="submit" className="w-fit" form={props.form}></input>
     </Button>
-  )
-}
+  );
+};
